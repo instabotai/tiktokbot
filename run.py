248a52a76d60d846e805f2799b3597150462b6f0
Steffan Jensen <6hourapp@gmail.com>
2019-11-27 15:38:48 -0800

    Update run.py

--- run.py
+++ run.py
@@ -5,55 +5,93 @@ screenWidth, screenHeight = pyautogui.size() # Returns two integers, the width a
 
 
 currentMouseX, currentMouseY = pyautogui.position() # Retursn two integes, the x and y of the mouse cursor's current position.
+class Bot():
+    
+    def __init__ (self, x=0):
+        self.x = x
+        self.home = pyautogui.locateOnScreen('img/home.png', confidence=0.9)
 
-#pyautogui.moveTo(380, 290)
+    def get_around_ads(self, sleep):
+            pyautogui.click(35, 140) # close left side
+            time.sleep(sleep)
+            pyautogui.click(35, 140) # close left side
+            time.sleep(0.7)
+            pyautogui.click(35, 140) # close left side
+            time.sleep(0.7)
 
-def get_around_ads(sleep):
-        pyautogui.click(20, 140) # close left side
+    def liker(sleep):
+        time.sleep(1)
+        liker = pyautogui.locateOnScreen('img/heart.png', confidence=0.9)
         time.sleep(sleep)
-        pyautogui.click(20, 140) # close left side
-        time.sleep(1)
 
-def liker(sleep):
-    time.sleep(1)
-    pyautogui.click(380, 290)
-#    pyautogui.click(40, 790)
-    time.sleep(sleep)
+    def comment_liker(self, sleep):
+            pyautogui.click(95, 10) # go back to tiktok app
+            time.sleep(0.6)
+            pyautogui.click(420, 450)
+            time.sleep(0.6)
+            i = 0
+            while i in range(2000):
+                like = pyautogui.locateOnScreen('like.png', confidence=0.9)
+                time.sleep(0.1)
+                pyautogui.click(like)
+                time.sleep(0)
+                pyautogui.scroll(-3)
+                time.sleep(0.4)
+                self.x += 1
+                print(str(self.x))
+                i += 1
+                if like == None:
+                    break
+            #like = pyautogui.locateOnScreen('like.png', confidence=0.9) 
+            pyautogui.click(like)
+            #pyautogui.click(410, 190)
+            time.sleep(0.3)
+            pyautogui.click(420, 120)
+            time.sleep(0.4)
+            self.get_around_ads(1)
+            time.sleep(sleep)
+            pyautogui.click(95, 10) # go back to tiktok app
+            pyautogui.click(40, 790)
+            time.sleep(0.3)
 
-def comment_liker(sleep):
-        pyautogui.click(420, 450)
+    def comment(self, sleep):
+            comment = ["this is cool", "yeaah", "hehe", "hehehe", ";)", ":)", "wow", "wooow", "nice", "cool", "awesome", "yes", "this is awesome", "agree"]
+            pyautogui.click(420, 450)
+            time.sleep(1.5)
+            pyautogui.click(10, 330)
+            time.sleep(1.5)
+            comment = random.choice(comment)
+            pyautogui.typewrite(comment)
+            time.sleep(0.5)
+            pyautogui.click(420, 790)
+            time.sleep(0.5)
+            pyautogui.click(420, 120) # close right side
+            pyautogui.click(20, 140) # close left side
+            time.sleep(0)
+            pyautogui.click(20, 140) # close left side
+            time.sleep(2)
+            pyautogui.click(40, 790)
+            time.sleep(1)
+            pyautogui.click(40, 790)
+            self.x += 1
+            print(str(self.x))
+            time.sleep(sleep)
+
+    def follow_user(self, sleep):
+        home = pyautogui.locateOnScreen('img/home.png', confidence=0.9)
+        time.sleep(0.1)
+        pyautogui.click(home)
         time.sleep(1)
-        pyautogui.click(410, 190)
-        time.sleep(1)
-        pyautogui.click(420, 120)
-        time.sleep(1)
-        get_around_ads(1)
+        follow_buttom = pyautogui.locateOnScreen('img/follow.png', confidence=0.9)
+        pyautogui.click(follow_buttom)
         time.sleep(sleep)
-        pyautogui.click(40, 790)
-        time.sleep(1)
+        self.x += 1
+        print(str(self.x))
 
-def comment(sleep):
-        comment = ["this is cool", "yeaah", "hehe", "hehehe", ";)", ":)", "wow", "wooow", "nice", "cool", "awesome", "yes", "this is awesome", "agree"]
-        pyautogui.click(420, 450)
-        time.sleep(0.5)
-        pyautogui.click(10, 330)
-        time.sleep(0.5)
-        comment = random.choice(comment)
-        pyautogui.typewrite(comment)
-        time.sleep(0.5)
-        pyautogui.click(420, 790)
-        time.sleep(0.5)
-        pyautogui.click(420, 120) # close right side
-        pyautogui.click(20, 140) # close left side
-        time.sleep(0)
-        pyautogui.click(20, 140) # close left side
-        time.sleep(2)
-        pyautogui.click(40, 790)
-        time.sleep(1)
-        pyautogui.click(40, 790)
-        time.sleep(2)
-
+bot = Bot()
 while True:
-    comment_liker(4)
+#    bot.comment_liker(1)
+    bot.follow_user(45)
+    #bot.comment(10)
     #comment(10)
-    liker(5)
+    #liker(5)
