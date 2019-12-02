329adc802fa01a79e11925f7b90a9d7f5688f547
Steffan Jensen <6hourapp@gmail.com>
2019-12-02 03:37:20 +0100

    now go out of more "errors" and better detection.

--- run.py
+++ run.py
@@ -20,10 +20,12 @@ class Bot():
             time.sleep(0.7)
 
     def liker(self, sleep):
-        time.sleep(1)
-        liker = pyautogui.locateOnScreen('img/heart.png', confidence=0.9)
+        time.sleep(2)
+        liker = pyautogui.locateOnScreen('img/heart.png', confidence=0.8)
         pyautogui.click(liker)
         time.sleep(sleep)
+        self.x += 1
+        print(str(self.x))
 
     def comment_liker(self, sleep):
             pyautogui.click(95, 10) # go back to tiktok app
@@ -55,45 +57,65 @@ class Bot():
             pyautogui.click(40, 790)
             time.sleep(0.3)
 
+    def confirm(self):
+        confirm = pyautogui.locateOnScreen('img/confirm.png', confidence=0.8)
+        pyautogui.click(confirm)
+
     def comment(self, sleep):
             comment = ["this is cool", "yeaah", "hehe", "hehehe", ";)", ":)", "wow", "wooow", "nice", "cool", "awesome", "yes", "this is awesome", "agree"]
-            comments = pyautogui.locateOnScreen('img/comment.png', confidence=0.9)
-            time.sleep(4)
-            pyautogui.click(comments)
-            time.sleep(4)
-            comment = random.choice(comment)
-            time.sleep(4)
-            add_comment = pyautogui.locateOnScreen('img/add_comment.png', confidence=0.9)
-            time.sleep(4)
-            pyautogui.click(add_comment)
             time.sleep(2)
+            comments = pyautogui.locateOnScreen('img/comment.png', confidence=0.8)
+            print("does comment box exist", comments)
+            time.sleep(1)
+            pyautogui.click(comments)
+            time.sleep(1)
+            comment = random.choice(comment)
+            time.sleep(1)
+            add_comment = pyautogui.locateOnScreen('img/add_comment.png', confidence=0.9)
+            time.sleep(1)
+            pyautogui.click(add_comment)
+            time.sleep(1)
             pyautogui.typewrite(comment)
             time.sleep(1)
             click = pyautogui.locateOnScreen('img/commentclick.png', confidence=0.9)
-            time.sleep(2)
-            pyautogui.click(click)
             time.sleep(1)
-            close = pyautogui.locateOnScreen('img/close.png', confidence=0.9)
+            pyautogui.click(click)
+            time.sleep(2)
+            close = pyautogui.locateOnScreen('img/close.png', confidence=0.8)
             time.sleep(2)
             pyautogui.click(close)
-            time.sleep(2)
+            time.sleep(1)
+   #         self.confirm()
             time.sleep(sleep)
+            self.x += 1
+            print(str(self.x))
+
+    def go_back_page(self):
+        go_back = pyautogui.locateOnScreen('img/back.png', confidence=0.9)
+        pyautogui.click(go_back)
+
 
     def follow_user(self, sleep):
-        home = pyautogui.locateOnScreen('img/home.png', confidence=0.9)
-        time.sleep(0.1)
-        pyautogui.click(home)
         time.sleep(1)
-        follow_buttom = pyautogui.locateOnScreen('img/follow.png', confidence=0.9)
+        follow_buttom = pyautogui.locateOnScreen('img/follow.png', confidence=0.8)
+        print("does follow buttom exist", follow_buttom)
         pyautogui.click(follow_buttom)
+        time.sleep(1)
+        self.go_back_page()
         time.sleep(sleep)
-        self.x += 1
-        print(str(self.x))
+
 
 bot = Bot()
 while True:
+    home = pyautogui.locateOnScreen('img/home.png', confidence=0.8)
+    print(home)
+    time.sleep(2)
+    pyautogui.click(home)
+    time.sleep(4)
     #bot.comment_liker(5)
-    bot.follow_user(2 + random.random())
+    bot.follow_user(65 + random.random())
+    time.sleep(10)
     #bot.comment(10)
-    bot.comment(2)
-    bot.liker(2 + random.random())
+    bot.comment(4)
+    time.sleep(3)
+   # bot.liker(1 + random.random())
